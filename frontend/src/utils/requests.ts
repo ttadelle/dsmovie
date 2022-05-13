43e636ccf922aa6eab42af38f37792ce72d221c0
Thiago Tadelle <thiago.tadelle@gmail.com>
2022-05-13 11:28:54 -0300

    Correcao Git 2

--- frontend/src/utils/requests.ts
+++ frontend/src/utils/requests.ts
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? "http://localhost:8080";
-=======
-export const BASE_URL = "http://localhost:8080";
->>>>>>> 6dedf73fd4ade1714d45a780121e5701cafdca56
