8f0178781de5ca179d5ce7208c9cac4fbe5a7fee
Thiago Tadelle <thiago.tadelle@gmail.com>
2022-05-13 22:30:19 -0300

    Pagination

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
 import { BASE_URL } from "utils/requests";
@@ -22,26 +22,29 @@ function Listing() {
     });
 
     useEffect(() => {
-        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}$sort=id`)
-        .then(response => {
-            const data = response.data as MoviePage;
-            setPage(data);
-        });
+        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=title`)
+            .then(response => {
+                const data = response.data as MoviePage;
+                setPage(data);
+            });
     }, [pageNumber]);
 
-    return (
+    const handlePageChange = (newPageNumber : number) => {
+        setPageNumber(newPageNumber);
+    }
 
+    return (
         <>
-            <Pagination />
+            <Pagination page={page} onChange={handlePageChange} />
 
             <div className="container">
                 <div className="row">
                     {page.content.map(movie => (
-                        <div key = {movie.id} className="col-sm-6 col-lg-4 col-xl-3">
-                        <MovieCard movie = {movie}/>
+                        <div key={movie.id} className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                            <MovieCard movie={movie} />
                         </div>
-                        )
-                    )}                    
+                    )
+                    )}
                 </div>
             </div>
         </>
