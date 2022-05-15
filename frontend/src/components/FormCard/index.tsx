0cbcad54022903c40f8ee2beee26b3f19c0626cc
Thiago Tadelle <68523966+ttadelle@users.noreply.github.com>
2022-05-15 08:45:14 -0300

    Update index.tsx

--- frontend/src/components/FormCard/index.tsx
+++ frontend/src/components/FormCard/index.tsx
@@ -1,7 +1,7 @@
 import axios, { AxiosRequestConfig } from 'axios';
 import { useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { Movie } from 'types/movie';
+import { Movie } from 'Types/movie';
 import { BASE_URL } from 'utils/requests';
 import { validateEmail } from 'utils/validate';
 import './styles.css';
